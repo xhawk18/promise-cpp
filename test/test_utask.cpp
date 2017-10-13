71419a41e58c3baf002e9426e4d6f82e12e34e6c
X-Hawk <xhawk18@gmail.com>
2017-10-14 02:33:33 +0800

    ...

--- test/test_utask.cpp
+++ test/test_utask.cpp
@@ -1,13 +1,14 @@
 #include "../promise/utask.hpp"
 
 #include <stdio.h>
+#include <functional>
 #define LINE do{printf("%d:%s\n", __LINE__, __func__);} while(0)
 
-void task1(TaskPool *task_pool, void *arg){
+void task1(TaskPool *task_pool){
     LINE;
 }
 
-void task2(TaskPool *task_pool, void *arg){
+void task2(TaskPool *task_pool){
     LINE;
     task_pool->run_next_task();
     LINE;
@@ -15,25 +16,32 @@ void task2(TaskPool *task_pool, void *arg){
     LINE;
 }
 
-void task3(TaskPool *task_pool, void *arg){
-    unsigned int id = task_pool->current_task_id();
-    for(int k = 0; k < 2; ++k){
-        printf("task id = %d, k = %d\n", id, k);
-        task_pool->run_next_task();
-    }
+void task_proxy(TaskPool *task_pool, void *arg){
+    const std::function<void(TaskPool *)> func
+        = *(const std::function<void(TaskPool *)> *)arg;
+    (func)(task_pool);
 }
 
+inline void create_task(TaskPool *task_pool, const std::function<void(TaskPool *)> &func){
+    task_pool->create_task(&task_proxy, (void *)&func);
+}
 
 int main(){
     TaskPool task_pool;
 
     LINE;
 
-    task_pool.create_task(&task1, NULL);
-    task_pool.create_task(&task2, NULL);    
+    create_task(&task_pool, task1);
+    create_task(&task_pool, task2);    
 
     for(int i = 0; i < 10; ++i)
-        task_pool.create_task(&task3, NULL);    
+        create_task(&task_pool, [=](TaskPool *task_pool){
+            unsigned int id = task_pool->current_task_id();
+            for(int k = 0; k < 200; ++k){
+                printf("task id = %d, i = %d, k = %d\n", id, i, k);
+                task_pool->run_next_task();
+            }
+        });
 
     LINE;
 
